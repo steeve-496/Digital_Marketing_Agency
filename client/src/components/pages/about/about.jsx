cbc22771ff87c9b7bd4bb907e27a5a0e53b7c832
Steeve <57051366+steeve-496@users.noreply.github.com>
2025-11-19 11:15:35 +0530

    Completed About

--- client/src/components/pages/about/about.jsx
+++ client/src/components/pages/about/about.jsx
@@ -5,15 +5,19 @@ import { useGSAP } from '@gsap/react';
 import {ScrollTrigger} from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+import DecryptedText from '../effects/decryptedText.jsx';
+
 function About(){
 
   const aboutRef= useRef();
-  const abouttextRef = useRef();
+
 
   useGSAP(()=> {
-    gsap.fromTo(".about-img",{
+
+
+    gsap.fromTo(aboutRef.current,{
       y:0,
-      width:10,
+      width:'10%',
       opacity:0,
       borderRadius: '50%',
     },{
@@ -21,59 +25,94 @@ function About(){
       opacity:1,
       width:'40%',
       duration: 1.5,
-      borderRadius: '10%',
+      borderRadius: '5%',
       scrollTrigger: {
         trigger:'.about-img',
         start:'bottom bottom',
         end:'top 20%',
         scrub:true,
         duration:2,
-        ease: "power1.inOut",
-
-      }
-      }
+        ease: "power1.inOut"
+      }      
+    }
     )
 
-    gsap.to(aboutRef.current,{
-      y:-20,
-      repeat:-1,
-      yoyo:true,
-      duration:1.5,
-    })
-
-    gsap.fromTo(abouttextRef.current,{
-        x:-20,
-        opacity:0,
-      },
-      {
-        x:0,
-        opacity:1,
-        trigger:".about",
-        scrollTrigger:{
-          start: 'bottom bottom',
-          end:'top 20%',
-          scrub: true,
-          markers:1,
-          duration:1,
-        ease: 'power1.inOut',
-        }
-    })
+    gsap.from(
+      ".about-img",{
+        y:-20,
+        repeat:-1,
+        yoyo:true,
+        duration:2,
+      }
+    )
 
 
   },[]);
 
 
+  useGSAP(() =>{
+
+    gsap.fromTo(".about-title",
+      {
+        y: 100, 
+        opacity: 0,
+      }
+      , {
+        y: 0, 
+        opacity: 1,
+        scrollTrigger:{
+          trigger:'.about',
+          start:'top 50%',
+          end: "top 50%",
+          duration:5,
+          markers:1,
+          scrub:true,
+          ease: 'power1.in'
+        }
+      }
+    )
+
+
+    gsap.fromTo(".about-desc",
+      {
+        x: -100, 
+        opacity: 0,
+      }
+      , {
+        x: 0, 
+        opacity: 1,
+        scrollTrigger:{
+          trigger:'.about',
+          start:'top 30%',
+          end: "bottom bottom",
+          delay:1,
+          duration:4,
+          stagger: 0.1,
+          markers:1,
+          scrub:true,
+          ease: 'power1.in'
+        }
+      }
+    )
+
+  },[]);
+
   return (
     <section className="about-section" id="about">
       <div className="about">
-        <div className="about-text" ref={abouttextRef}>
-          <h2 className="about-title texts">ABOUT <br /> SYSDEVCODE</h2>
-          <p className="about-desc texts">
-            We are creative developers that transforms innovative ideas into
-            powerful digital experiences. Out team combines artistic vision with
-            technical expertise to deliver solutions that captivate and convert
-          </p>
-        </div>
+        <div className="about-text">
+          <h2 className="about-title">ABOUT <br /> SYSDEVCODE</h2>  
+          <div style={{ marginTop: '4rem' }} className="about-desc">
+
+          <DecryptedText  className="about-desc-span"
+              text="We are creative developers that transforms innovative ideas into powerful digital experiences. Out team combines artistic vision with technical expertise to deliver solutions that captivate and convert"
+              animateOn="view"
+              revealDirection="start"
+              speed={40}
+              maxIterations={6}
+          />
+          </div>
+        </div>        
           <img ref={aboutRef} src="/about-img.png" alt="about-img" className="about-img" />
       </div>
     </section>
